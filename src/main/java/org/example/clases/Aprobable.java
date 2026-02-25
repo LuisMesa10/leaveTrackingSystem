add63fb5ad36139082ee3dceac9003e52a662f2b
Luis Mesa <luis_mesa82212@elpoli.edu.co>
2026-02-25 14:55:49 -0500

    refactor: mejora en la interfaz Aprobable y estandarización de métodos de aprobación

--- src/main/java/org/example/clases/Aprobable.java
+++ src/main/java/org/example/clases/Aprobable.java
@@ -3,5 +3,5 @@ package org.example.clases;
 public interface Aprobable {
 
     boolean aprobado(String nombreAprobador);
-    boolean denegeado(String nombreAprobador, String razon);
+    boolean denegado(String nombreAprobador, String razon);
 }
